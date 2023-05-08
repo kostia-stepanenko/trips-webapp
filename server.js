f60d6f6bea48a907167b0d3a575bef86f9975861
mstepan <maksym.stepanenko@oracle.com>
2023-05-08 15:25:32 +0300

    Logout flow added

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const expressHandlebars = require('express-handlebars').engine;
 const handlers = require('./lib/handlers')
 
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const expressSession = require('express-session');
 
 const app = express();
@@ -13,26 +13,37 @@ app.engine('handlebars', expressHandlebars({
 }));
 app.set('view engine', 'handlebars');
 
-// static content: images, JS scripts and CSS
-console.log(`Static content location: ${__dirname + '/public'}`);
-app.use(express.static(__dirname + '/public'));
-
-app.use(cookieParser('my-secret'));
+// cookie parser middleware
+app.use(cookieParser());
 
 // session middleware
 app.use(expressSession({
-  resave: false,
-  saveUninitialized: false,
   secret: 'my-secret',
+  saveUninitialized: true,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 },
+  resave: false
 }));
+
+// middleware to propagate session information to view
+app.use(function (req, res, next) {
+  res.locals.session = req.session;
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// static content: images, JS scripts and CSS
+console.log(`Static content location: ${__dirname + '/public'}`);
+app.use(express.static(__dirname + '/public'));
 
 // home page handler
 app.get('/', handlers.home);
 
 // login page handler
 app.get('/login', handlers.login);
+
+// logout page handler
+app.get('/logout', handlers.logout);
 
 // authentication flow
 app.post('/auth', handlers.auth);
@@ -57,5 +68,5 @@ const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(
   `trips-webapp server started at http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`
+  `press Ctrl-C to terminate...`
 ));
